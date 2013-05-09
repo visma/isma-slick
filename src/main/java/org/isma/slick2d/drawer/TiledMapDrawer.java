408ddd12bc2fccd6a49803824d3397eb882fb334
ismael mameche <ismael.romathier@gmail.com>
2013-05-09 13:08:43 +0200

    sonar : cycles eradication

--- src/main/java/org/isma/slick2d/drawer/TiledMapDrawer.java
+++ src/main/java/org/isma/slick2d/drawer/TiledMapDrawer.java
@@ -1,17 +1,19 @@
 package org.isma.slick2d.drawer;
 
 import org.isma.slick2d.GameContext;
+import org.isma.slick2d.tileset.TiledObject;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
-public class TiledMapDrawer<O, G extends GameContext> extends AbstractDrawer<O, G> {
+public class TiledMapDrawer<T extends TiledObject, G extends GameContext> extends AbstractDrawer<T, G> {
     protected TiledMapDrawer(G context) {
         super(context);
     }
 
 
-    public void draw(Graphics g, TiledMap tiledMap, int layerIndex) throws SlickException {
+    public void draw(Graphics g, T tiledObject, int layerIndex) throws SlickException {
+        TiledMap tiledMap = tiledObject.getTiledMap();
         tiledMap.render(0, 0, 0, 0, tiledMap.getWidth(), tiledMap.getHeight(), layerIndex, true);
     }
 
